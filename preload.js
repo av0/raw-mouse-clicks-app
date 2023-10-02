9cb46f13b78c77f8fd03d935a0807e8bfa0f681c
av0 <av.0@outlook.com>
2023-10-02 17:48:53 +0300

    Device change events

--- preload.js
+++ preload.js
@@ -4,5 +4,7 @@ contextBridge.exposeInMainWorld('rawInputAPI', {
     onRawMouseButtonClick: (callback) => ipcRenderer.on('raw-mouse-button-click', callback),
     onRawMouseButtonUp: (callback) => ipcRenderer.on('raw-mouse-button-up', callback),
     onRawMouseButtonDown: (callback) => ipcRenderer.on('raw-mouse-button-down', callback),
+    onRawMouseDeviceAdded: (callback) => ipcRenderer.on('raw-mouse-device-added', callback),
+    onRawMouseDeviceRemoved: (callback) => ipcRenderer.on('raw-mouse-device-removed', callback),
     getMouseDevices: () => ipcRenderer.invoke('rawinput::getMouseDevices'),
 })
